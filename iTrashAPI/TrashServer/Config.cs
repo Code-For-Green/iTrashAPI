81c72175bbfd1621a95d35512da8c380237bf29b
Crawcik <crawcik@gmail.com>
2021-05-04 11:28:24 +0200

    Config added

---
+++ iTrashAPI/TrashServer/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace TrashServer
+{
+    public class Config
+    {
+        public string EndPoint { get; private set; } = "/relay/";
+        public ushort Port { get; private set; } = 8080;
+    }
+}
--- iTrashAPI/TrashServer/Runtime.cs
+++ iTrashAPI/TrashServer/Runtime.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using TrashServer;
+
+const string path = "config.json";
+Config config;
+
+//Checking config
+if (!File.Exists(path))
+{
+    File.WriteAllText(path, JsonSerializer.Serialize<Config>(new()));
+    return;
+}
+try
+{
+    config = JsonSerializer.Deserialize<Config>(File.ReadAllText(path));
+}
+catch (IOException)
+{
+    Console.WriteLine("File is currently handled by another application");
+    return;
+}
+catch (JsonException)
+{
+    Console.WriteLine("The file architecture is invalid");
+    return;
+}
+if (config is null)
+{
+    Console.WriteLine("Config is empty");
+    return;
+}
