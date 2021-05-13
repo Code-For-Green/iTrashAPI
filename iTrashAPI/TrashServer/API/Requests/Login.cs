9362f4be5891e784bf8501fa87feb4039f6839a3
Crawcik <crawcik@gmail.com>
2021-05-13 23:17:26 +0200

    Change Expirtation to timestamp

--- iTrashAPI/TrashServer/API/Requests/Login.cs
+++ iTrashAPI/TrashServer/API/Requests/Login.cs
@@ -7,14 +7,16 @@ namespace TrashServer.API.Requests
     [RequestKey("login")]
     public class Login : IRequest
     {
-        private Random _random = new();
+        private readonly Random _random = new();
         public Task Execute(string json, out string response)
         {
             User user = JsonSerializer.Deserialize<User>(json);
             User hashedUser = user.Hashed();
             if (!Database.UserList.Contains(hashedUser))
                 throw new Exception("Wrong!");
-            UserToken userToken = new UserToken { Token = GenerateToken(), Expirity = (int)Math.Round(TimeSpan.FromMinutes(10).TotalSeconds) };
+            DateTime foo = DateTime.UtcNow.AddMinutes(10);
+            long unixTime = ((DateTimeOffset)foo).ToUnixTimeSeconds();
+            UserToken userToken = new UserToken { Token = GenerateToken(), Expiration = unixTime };
             response = JsonSerializer.Serialize(userToken);
             return Task.CompletedTask;
         }
