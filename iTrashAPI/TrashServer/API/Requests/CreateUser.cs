bfbd381365f9413e79e4176ab85dedd8511590f5
Crawcik <crawcik@gmail.com>
2021-05-13 22:27:34 +0200

    New request keys

---
+++ iTrashAPI/TrashServer/API/Requests/CreateUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TrashServer.API.Requests
+{
+    [RequestKey("create_user")]
+    public class CreateUser : IRequest
+    {
+        public Task Execute(string json, out string response)
+        {
+            User user = JsonSerializer.Deserialize<User>(json);
+            User hashedUser = user.Hashed();
+            Database.UserList.Add(hashedUser);
+            response = "OK";
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ iTrashAPI/TrashServer/API/Requests/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TrashServer.API.Requests
+{
+    [RequestKey("login")]
+    public class Login : IRequest
+    {
+        private Random _random = new();
+        public Task Execute(string json, out string response)
+        {
+            User user = JsonSerializer.Deserialize<User>(json);
+            User hashedUser = user.Hashed();
+            if (!Database.UserList.Contains(hashedUser))
+                throw new Exception("Wrong!");
+            UserToken userToken = new UserToken { Token = GenerateToken(), Expirity = (int)Math.Round(TimeSpan.FromMinutes(10).TotalSeconds) };
+            response = JsonSerializer.Serialize(userToken);
+            return Task.CompletedTask;
+        }
+
+        private string GenerateToken()
+        {
+            byte[] result = new byte[8];
+            for(int i = 0; i < 8; i++ )
+                 result[i] = (byte)_random.Next(byte.MaxValue);
+            return Convert.ToBase64String(result);
+        }
+    }
+}
